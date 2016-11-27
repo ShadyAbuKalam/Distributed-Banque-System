96619e2b79ffb47fa024ae999b15f65042750066
Shady Atef <shadyoatef@gmail.com>
2016-11-27 19:30:14 +0200

    Corrected External transaction definition

--- src/main/resources/db/migration/V4__Create_External_Transaction_Table.sql
+++ src/main/resources/db/migration/V4__Create_External_Transaction_Table.sql
@@ -6,7 +6,7 @@ create table ExternalTransaction (
   internal_user VARCHAR(30) not null PRIMARY KEY ,
   timestmap TIMESTAMP not null  PRIMARY KEY ,
   external_user VARCHAR(30) not null ,
-  internal_user VARCHAR(30) not null ,
+  external_bank VARCHAR(30) not null ,
   FOREIGN KEY  (internal_user,timestmap) REFERENCES Transactions (user_name,timestmap) ON UPDATE CASCADE ON DELETE RESTRICT,
-  FOREIGN KEY (internal_user) REFERENCES Banks (name) ON UPDATE CASCADE ON DELETE RESTRICT
+  FOREIGN KEY (external_bank) REFERENCES Banks (name) ON UPDATE CASCADE ON DELETE RESTRICT
 );
